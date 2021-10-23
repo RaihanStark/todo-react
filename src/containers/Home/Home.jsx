4f13b322231318a95e5f86e13c5d7547cc41cada
Raihan Stark <raihanyudosaputra30@gmail.com>
2021-10-23 13:27:00 +0700

    fix toggle bug

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,7 +5,6 @@ import TaskList from "../TaskList/TaskList";
 import BottomNavigation from "../BottomNavigation/BottomNavigation";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleTask, deleteTask } from "../../redux";
-
 const StyledHome = styled.div`
   background-color: #f4f7fd;
   height: 100vh;
@@ -18,23 +17,31 @@ const StyledSectionTitle = styled.h2`
 function Home() {
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-
+  console.log(tasks);
   return (
     <StyledHome>
       <Container>
         <StyledSectionTitle>Tasks</StyledSectionTitle>
         <TaskList>
-          {tasks.map((task) => {
-            return (
-              <Task
-                key={task.id}
-                title={task.title}
-                checked={task.isChecked}
-                onChange={() => dispatch(toggleTask(task.id))}
-                onDelete={() => dispatch(deleteTask(task.id))}
-              ></Task>
-            );
-          })}
+          {tasks.length !== 0 ? (
+            tasks.map((task, index) => {
+              return (
+                <Task
+                  key={index}
+                  title={task.title}
+                  checked={task.isChecked}
+                  onChange={() => dispatch(toggleTask(index))}
+                  onDelete={() => dispatch(deleteTask(index))}
+                ></Task>
+              );
+            })
+          ) : (
+            <h3
+              style={{ color: "#929292", fontWeight: 400, textAlign: "center" }}
+            >
+              There's nothing to do!
+            </h3>
+          )}
         </TaskList>
       </Container>
       <BottomNavigation />
--- src/redux/task/taskReducer.js
+++ src/redux/task/taskReducer.js
@@ -1,29 +1,12 @@
 import { ADD_TASK, TOGGLE_TASK, DELETE_TASK } from "./taskTypes";
 
 const initialState = {
-  tasks: [
-    {
-      id: 0,
-      title: "Jump for one minutes",
-      isChecked: false,
-    },
-    {
-      id: 1,
-      title: "Jump for two minutes",
-      isChecked: false,
-    },
-    {
-      id: 2,
-      title: "Jump for three minutes",
-      isChecked: false,
-    },
-  ],
+  tasks: [],
 };
 
 const taskReducer = (state = initialState, actions) => {
   switch (actions.type) {
     case ADD_TASK:
-      console.log(actions.type, actions.payload);
       return {
         ...state,
         tasks: [
@@ -37,22 +20,16 @@ const taskReducer = (state = initialState, actions) => {
       };
 
     case TOGGLE_TASK:
-      let toggled = state.tasks.map((task) => {
-        if (task.id === actions.payload) {
-          task.isChecked = !task.isChecked;
-          return task;
-        }
-
-        return task;
-      });
-      return { ...state, tasks: toggled };
+      let toggled = state.tasks;
+      toggled[actions.payload].isChecked = !toggled[actions.payload].isChecked;
+      return { ...state, tasks: [...toggled] };
 
     case DELETE_TASK:
       return {
         ...state,
         tasks: [
-          ...state.tasks.filter((task) => {
-            return task.id !== actions.payload;
+          ...state.tasks.filter((task, index) => {
+            return index !== actions.payload;
           }),
         ],
       };
